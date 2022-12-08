ef648278ee01063e3358e18fab52d1e8f90ea44d
Mustafa80baz <112826649+Mustafa80baz@users.noreply.github.com>
2022-12-08 19:53:46 +0300

    tester topladı

---
+++ src/Chaptertwo2_2/tester.java
@@ -0,0 +1,10 @@
+package Chaptertwo2_2;
+
+public class tester {
+    public static void main(String[] args) {
+        int a=3;
+         int b=5;
+         int to=a+b;
+        System.out.println(to);
+    }
+}
